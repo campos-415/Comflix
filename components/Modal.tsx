5ee0e2247a4a8a1e9ff2481a4fba8831d0a28248
Cesar Campos <camposss.415@gmail.com>
2023-03-31 12:37:49 -0700

    match percet fixed

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -138,7 +138,7 @@ function Modal() {
           <div className="space-y-6 text-lg">
             <div className="flex items-center space-x-2 text-sm">
               <p className="font-semibold text-green-400">
-                {movie?.vote_average * 10}% Match
+                {Math.floor((movie?.vote_average * 10))}% Match
               </p>
               <p className="font-light ">
                 {movie?.release_date || movie?.first_air_date}
